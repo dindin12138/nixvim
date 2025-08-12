a741a917292fc2cea82ca3a7a3b803e9d66aada9
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-08-12 23:01:28 +0100

    feat: update telescope settings

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -7,40 +7,43 @@
         settings = { cmd = [ "Telescope" ]; };
       };
       settings = {
-        layout_config = {
-          horizontal = {
-            prompt_position = "top";
-            results_width = 0.6;
+        defaults = {
+          layout_config = {
+            horizontal = {
+              prompt_position = "top";
+              results_width = 0.6;
+            };
+            vertical = { mirror = false; };
           };
-          vertical = { mirror = false; };
-        };
-        mappings = {
-          i = {
-            "<C-j>" = {
-              __raw = "require('telescope.actions').move_selection_next";
+          sorting_strategy = "ascending";
+          mappings = {
+            i = {
+              "<C-j>" = {
+                __raw = "require('telescope.actions').move_selection_next";
+              };
+              "<C-k>" = {
+                __raw = "require('telescope.actions').move_selection_previous";
+              };
+              "<Down>" = {
+                __raw = "require('telescope.actions').move_selection_next";
+              };
+              "<Up>" = {
+                __raw = "require('telescope.actions').move_selection_previous";
+              };
+              "<C-n>" = {
+                __raw = "require('telescope.actions').cycle_history_next";
+              };
+              "<C-p>" = {
+                __raw = "require('telescope.actions').cycle_history_prev";
+              };
+              "<C-u>" = {
+                __raw = "require('telescope.actions').preview_scrolling_up";
+              };
+              "<C-d>" = {
+                __raw = "require('telescope.actions').preview_scrolling_down";
+              };
+              "<C-c>" = { __raw = "require('telescope.actions').close"; };
             };
-            "<C-k>" = {
-              __raw = "require('telescope.actions').move_selection_previous";
-            };
-            "<Down>" = {
-              __raw = "require('telescope.actions').move_selection_next";
-            };
-            "<Up>" = {
-              __raw = "require('telescope.actions').move_selection_previous";
-            };
-            "<C-n>" = {
-              __raw = "require('telescope.actions').cycle_history_next";
-            };
-            "<C-p>" = {
-              __raw = "require('telescope.actions').cycle_history_prev";
-            };
-            "<C-u>" = {
-              __raw = "require('telescope.actions').preview_scrolling_up";
-            };
-            "<C-d>" = {
-              __raw = "require('telescope.actions').preview_scrolling_down";
-            };
-            "<C-c>" = { __raw = "require('telescope.actions').close"; };
           };
         };
       };
