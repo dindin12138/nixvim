0888d5ba05aa2b48026e7883012bfff51d5182ef
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-08-15 15:56:36 +0100

    feat: add some formatter

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -1,5 +1,15 @@
 { pkgs, ... }: {
-  extraPackages = with pkgs; [ stylua nixfmt ];
+  extraPackages = with pkgs; [
+    # Formatter
+    stylua
+    nixfmt
+    prettier
+    shfmt
+    gofumpt
+    jq
+    cmake-format
+    sql-formatter
+  ];
   plugins = {
     conform-nvim = {
       enable = true;
@@ -7,6 +17,27 @@
         formatters_by_ft = {
           lua = [ "stylua" ];
           nix = [ "nixfmt" ];
+          c = [ "clang_format" ];
+          cpp = [ "clang_format" ];
+          cmake = [ "cmake_format" ];
+          go = [ "gofumpt" ];
+          sh = [ "shfmt" ];
+          json = [ "jq" ];
+          jsonc = [ "prettier" ];
+          sql = [ "sql_formatter" ];
+          javascript = [ "prettier" ];
+          javascriptreact = [ "prettier" ];
+          typescript = [ "prettier" ];
+          typescriptreact = [ "prettier" ];
+          vue = [ "prettier" ];
+          css = [ "prettier" ];
+          scss = [ "prettier" ];
+          less = [ "prettier" ];
+          html = [ "prettier" ];
+          yaml = [ "prettier" ];
+          markdown = [ "prettier" ];
+          graphql = [ "prettier" ];
+          handlebars = [ "prettier" ];
         };
         format_on_save = {
           lspFallback = true;
