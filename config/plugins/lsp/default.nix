3e2a373adeffb3a745a1a3e9f71aeee01c5bd206
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-09-18 20:45:22 +0800

    feat: update onAttach

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -13,7 +13,34 @@
       enable = true;
       # capabilities = true;
       inlayHints = true;
-      # onAttach = "";
+      onAttach = ''
+        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+        -- Highlight symbol under cursor
+        if client.server_capabilities.documentHighlightProvider then
+          vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
+          vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_document_highlight" })
+          vim.api.nvim_create_autocmd(
+            { "CursorHold", "CursorHoldI" },
+            { group = "lsp_document_highlight", buffer = bufnr, callback = vim.lsp.buf.document_highlight }
+          )
+          vim.api.nvim_create_autocmd(
+            { "CursorMoved", "CursorMovedI" },
+            { group = "lsp_document_highlight", buffer = bufnr, callback = vim.lsp.buf.clear_references }
+          )
+        end
+        -- Disable hover in favor of Pyright
+        if client.name == "ruff" then
+          client.server_capabilities.hoverProvider = false
+        end
+      '';
     };
   };
+  extraConfigLua = ''
+    local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+      opts = opts or {}
+      opts.border = opts.border or "rounded"
+      return orig_util_open_floating_preview(contents, syntax, opts, ...)
+    end
+  '';
 }
