8c755b11bc2b31a312236edccc0665410a6d53cd
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-10-26 21:45:46 +0800

    feat: use auto-session

---
+++ config/plugins/auto-session.nix
@@ -0,0 +1,15 @@
+{
+  plugins = {
+    auto-session = {
+      enable = true;
+      settings = {
+        bypass_save_filetypes = [
+          "alpha"
+          "dashboard"
+          "snacks_dashboard"
+        ];
+        use_git_branch = true;
+      };
+    };
+  };
+}
