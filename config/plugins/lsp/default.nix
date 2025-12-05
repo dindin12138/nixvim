7148bd5af94030a8943f1c87d298f87f61c410c1
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-12-05 12:47:05 +0800

    fix: deprecated vim func

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -50,17 +50,14 @@
 
     vim.diagnostic.config({
       virtual_text = true,
-      signs = true,
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.HINT] = "󰛩 ",
+          [vim.diagnostic.severity.INFO] = " ",
+        },
+      },
     })
-    local signs = {
-      Error = " ",
-      Warn = " ",
-      Hint = "󰛩 ",
-      Info = " ",
-    }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
   '';
 }
--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -64,19 +64,17 @@
               __unkeyed-1 = {
                 __raw = ''
                   function()
-                      local msg = ""
-                      local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-                      local clients = vim.lsp.get_active_clients()
-                      if next(clients) == nil then
-                          return msg
-                      end
-                      for _, client in ipairs(clients) do
-                          local filetypes = client.config.filetypes
-                          if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                              return client.name
-                          end
-                      end
-                      return msg
+                    local msg = ""
+                    local clients = vim.lsp.get_clients({ bufnr = 0 })
+                    if next(clients) == nil then
+                        return msg
+                    end
+                    for _, client in ipairs(clients) do
+                        if client.name ~= "copilot" then
+                            return client.name
+                        end
+                    end
+                    return msg
                   end
                 '';
               };
