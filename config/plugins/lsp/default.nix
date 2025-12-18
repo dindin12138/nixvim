c72f5587f3986df1e337fc5f7c58d8eaff1590d8
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-12-19 02:02:26 +0800

    feat: update lsp config

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -10,47 +10,45 @@
     ./bash.nix
   ];
 
-  plugins = {
-    lsp = {
-      enable = true;
-      # capabilities = true;
-      inlayHints = true;
-      onAttach = ''
-        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-        -- Highlight symbol under cursor
-        if client.server_capabilities.documentHighlightProvider then
-          vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
-          vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_document_highlight" })
-          vim.api.nvim_create_autocmd(
-            { "CursorHold", "CursorHoldI" },
-            { group = "lsp_document_highlight", buffer = bufnr, callback = vim.lsp.buf.document_highlight }
-          )
-          vim.api.nvim_create_autocmd(
-            { "CursorMoved", "CursorMovedI" },
-            { group = "lsp_document_highlight", buffer = bufnr, callback = vim.lsp.buf.clear_references }
-          )
-        end
-      '';
-    };
-  };
-  extraConfigLua = ''
-    local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-      opts = opts or {}
-      opts.border = opts.border or "rounded"
-      return orig_util_open_floating_preview(contents, syntax, opts, ...)
-    end
+  plugins.lsp = {
+    enable = true;
+    # capabilities = true;
+    inlayHints = true;
+    onAttach = ''
+      vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+      -- Highlight symbol under cursor
+      if client.server_capabilities.documentHighlightProvider then
+        vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
+        vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_document_highlight" })
+        vim.api.nvim_create_autocmd(
+          { "CursorHold", "CursorHoldI" },
+          { group = "lsp_document_highlight", buffer = bufnr, callback = vim.lsp.buf.document_highlight }
+        )
+        vim.api.nvim_create_autocmd(
+          { "CursorMoved", "CursorMovedI" },
+          { group = "lsp_document_highlight", buffer = bufnr, callback = vim.lsp.buf.clear_references }
+        )
+      end
+    '';
+    luaConfig.post = ''
+      local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+      function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+        opts = opts or {}
+        opts.border = opts.border or "rounded"
+        return orig_util_open_floating_preview(contents, syntax, opts, ...)
+      end
 
-    vim.diagnostic.config({
-      virtual_text = true,
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = " ",
-          [vim.diagnostic.severity.WARN] = " ",
-          [vim.diagnostic.severity.HINT] = "󰛩 ",
-          [vim.diagnostic.severity.INFO] = " ",
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.HINT] = "󰛩 ",
+            [vim.diagnostic.severity.INFO] = " ",
+          },
         },
-      },
-    })
-  '';
+      })
+    '';
+  };
 }
