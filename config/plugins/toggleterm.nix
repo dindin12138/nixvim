906cf5a2bb61237aa92d5cc1371ade01817ff334
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-12-19 10:54:25 +0800

    feat: add lazygit

--- config/plugins/toggleterm.nix
+++ config/plugins/toggleterm.nix
@@ -1,9 +1,48 @@
 {
   plugins.toggleterm = {
     enable = true;
-    lazyLoad.settings.cmd = [ "ToggleTerm" ];
+    lazyLoad.settings = {
+      cmd = [ "ToggleTerm" ];
+      keys = [
+        {
+          __unkeyed-1 = "<leader>t";
+          __unkeyed-2 = "<cmd>lua _lazygit_toggle()<CR>";
+          mode = "n";
+          desc = "Lazygit";
+        }
+      ];
+    };
     settings = {
       open_mapping = "[[<c-\\>]]";
+      float_opts = {
+        border = "curved";
+      };
+      highlights = {
+        FloatBorder = {
+          link = "FloatBorder";
+        };
+        NormalFloat = {
+          link = "NormalFloat";
+        };
+      };
     };
+    luaConfig.post = ''
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new({
+        cmd = "lazygit",
+        dir = "git_dir",
+        direction = "float",
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+        end,
+        on_close = function(term)
+          vim.cmd("startinsert!")
+        end,
+      })
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+    '';
   };
 }
