56272c83f78f7cc4e8b6b18eb2c2ba7baab046bc
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2026-02-03 20:27:20 +0800

    feat(snacks): enable bigfile, quickfile, words

--- config/plugins/snacks.nix
+++ config/plugins/snacks.nix
@@ -3,6 +3,9 @@
     enable = true;
     settings = {
       input.enabled = true;
+      bigfile.enabled = true;
+      quickfile.enabled = true;
+      words.enabled = true;
       picker = {
         enabled = true;
         ui_select = true;
