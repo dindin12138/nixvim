5e494b8efec5f862bcf2620d8dd4397f9e4f56d5
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2026-03-03 19:34:50 +0800

    update(nvim-surround): migrate v3 to v4

--- config/plugins/nvim-surround.nix
+++ config/plugins/nvim-surround.nix
@@ -5,10 +5,48 @@
       "BufReadPost"
       "BufNewFile"
     ];
-    settings = {
-      keymaps = {
-        visual = "gs";
-      };
-    };
   };
+  plugins.which-key.settings.spec = [
+    {
+      __unkeyed-1 = "ys";
+      mode = "n";
+      desc = "Add Surround";
+      icon = "";
+    }
+    {
+      __unkeyed-1 = "ds";
+      mode = "n";
+      desc = "Delete Surround";
+      icon = "";
+    }
+    {
+      __unkeyed-1 = "cs";
+      mode = "n";
+      desc = "Change Surround";
+      icon = "";
+    }
+    {
+      __unkeyed-1 = "yss";
+      mode = "n";
+      desc = "Add Surround Line";
+      icon = "";
+    }
+    {
+      __unkeyed-1 = "gs";
+      mode = "x";
+      desc = "Add Surround (Visual)";
+      icon = "";
+    }
+  ];
+  keymaps = [
+    {
+      mode = "x";
+      key = "gs";
+      action = "<Plug>(nvim-surround-visual)";
+      options = {
+        desc = "Add a surrounding pair around a visual selection";
+        silent = true;
+      };
+    }
+  ];
 }
